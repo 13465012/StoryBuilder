8fe6d2b9a8c25a5c8c5367c4c2a8c83c306c55c0
James <contact@jhilton.co.uk>
2016-08-12 23:53:39 +0100

    Syntax overlaytest
    * removed red test colors
    * introduced testing for word 'bold'
    * introduced overlay side scrolling for side scroll bug caused by added lots of whitespace in the text area
    * Added licence to main-renderer.js

--- src/windows/main/main-renderer.js
+++ src/windows/main/main-renderer.js
@@ -1,3 +1,29 @@
+/* License Declaration
+MIT License
+
+Copyright (c) 2016 James Hilton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+
 window.$ = window.jQuery = require("jquery");
 
 // onload
@@ -8,14 +34,22 @@ $(document).ready(function() {
 		// check that space has been pressed
 
 		// get contents of text area
-		aTXT = $("#main").val();
+		var aTXT = $("#main").val();
 		aTXT += "<br />";
+
+		// apply test styling
+		aTXT = aTXT.split("bold");
+		aTXT = aTXT.join("<b>bold</b>");
+
 		// copy contents to syntax
 		$("#mainSyntax").html(aTXT);
 
 		// adjust syntax to main scroll
 		var y = $("#main").scrollTop();
 		$("#mainSyntax").scrollTop(y);
+		// adjyst syntax to scroll left;
+		var y = $("#main").scrollLeft();
+		$("#mainSyntax").scrollLeft(y);
 
 	});
 
@@ -23,5 +57,9 @@ $(document).ready(function() {
 		// adjust syntax to main scroll
 		var y = $("#main").scrollTop();
 		$("#mainSyntax").scrollTop(y);
+
+		// adjyst syntax to scroll left;
+		var y = $("#main").scrollLeft();
+		$("#mainSyntax").scrollLeft(y);
 	});
 });
